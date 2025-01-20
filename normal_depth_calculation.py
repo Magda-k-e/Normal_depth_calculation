5b97e50e229b0f09c75c4530e415c79711adbda5
Magdalini Kentroti <magda.kentr@gmail.com>
2025-01-20 19:40:34 +0200

    depth calculation

--- normal_depth_calculation.py
+++ normal_depth_calculation.py
@@ -48,6 +48,42 @@ class MyApp():
         self.width_entry = tk.Entry(self.root, font=self.fnt, width=20, bg='lightgreen', fg='blue')
         self.width_entry.pack(fill='both', expand=1)
 
+        # calculate button
+        self.submit_button = tk.Button(self.root, text="Calculate", font=self.fnt, command=self.calculate_dep)
+        self.submit_button.pack(pady=20)
+
+        self.result_label = tk.Label(self.root, text="Calculated normal depth:", font=self.fnt, fg='green')
+        self.result_label.pack(pady=10)
+
+    def calculate_dep(self):
+        # get each value entered in each Entry
+
+        self.slope = float(self.slope_entry.get())
+        self.manning = float(self.manning_entry.get())
+        self.flow = float(self.flow_entry.get())
+        self.width = float(self.width_entry.get())
+
+        self.product_qnj = self.flow * self.manning / self.slope ** (1 / 2)
+
+        self.product_by = (self.width * self.norm_dep) * (
+                (self.width * self.norm_dep) / (self.width + 2 * self.norm_dep)) ** (2 / 3)
+
+        tolerance = 1e-2
+
+        while abs(self.product_by - self.product_qnj) > tolerance:
+            if self.product_by > self.product_qnj:
+                self.norm_dep -= 0.005
+            else:
+                self.norm_dep += 0.005
+
+            # calculate product_by with new normal depth
+            self.product_by = (self.width * self.norm_dep) * (
+                    (self.width * self.norm_dep) / (self.width + 2 * self.norm_dep)) ** (2 / 3)
+
+            self.root.update_idletasks()
+
+            self.result_label.config(text=f"Calculated normal depth:{self.norm_dep:.2f}")
+
 
 root = tk.Tk()
 myapp = MyApp(root)
